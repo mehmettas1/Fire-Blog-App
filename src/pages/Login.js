fe112acdad92790d01a6a4baa6c41e06fb61405e
mehmet <tas349873@gmail.com>
2022-08-02 21:15:03 +0300

    login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,74 @@
-import React from 'react'
+import * as React from "react";
+import Box from "@mui/joy/Box";
+import TextField from "@mui/joy/TextField";
+import Button from "@mui/joy/Button";
+import { Avatar, Container, Grid, Typography } from "@mui/material";
+import blokPng from "../assets/blok.png";
+import googlePng from "../assets/google.png";
 
-const Login = () => {
+export default function TextFieldColors() {
   return (
-    <div>Login</div>
-  )
-}
+    <Container className="login-container">
+      <Box
+        sx={{
+          py: 2,
+          display: "grid",
+          gap: 2,
+          alignItems: "center",
+          flexWrap: "wrap",
+        }}
+      >
+        <Avatar
+          src={blokPng}
+          sx={{ width: "156px", height: "156px", mx: "auto" }}
+          alt="avatar-img"
+        />
+        <Typography
+          variant="h4"
+          component="h1"
+          sx={{ m: 4, fontFamily: "Girassol", color: "#046582" }}
+        >
+          ── Login ──
+        </Typography>
+        <TextField placeholder="Email" variant="outlined" color="danger" />
 
-export default Login
+        <TextField placeholder="Password" variant="outlined" color="danger" />
+        <Grid item xs={12}>
+          <Button
+            style={{ backgroundColor: "#046582", fontWeight: 700 }}
+            variant="contained"
+            color="primary"
+            type="submit"
+            // onClick={handleLogin}
+            fullWidth
+          >
+            Login
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Button
+            style={{
+              backgroundColor: "white",
+              color: "black",
+              fontWeight: 600,
+              fontSize: "0.9rem"
+              ,background:'grey'
+            }}
+            variant="contained"
+            color="secondary"
+            type="submit"
+          
+            fullWidth
+          >
+            With{" "}
+            <img
+              src={googlePng}
+              alt="google"
+              style={{ width: "80px", marginLeft: "10px" }}
+            />
+          </Button>
+        </Grid>
+      </Box>
+    </Container>
+  );
+}
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import * as React from 'react';
+import Box from '@mui/joy/Box';
+import TextField from '@mui/joy/TextField';
+import Button from '@mui/joy/Button';
 
-const Register = () => {
+export default function TextFieldColors() {
   return (
-    <div>Register</div>
-  )
+    <Box
+      sx={{
+        py: 2,
+        display: 'grid',
+        gap: 2,
+        alignItems: 'center',
+        flexWrap: 'wrap',
+      }}
+    >
+     
+      
+      <TextField
+        placeholder="Email"
+        variant="outlined"
+        color="danger"
+      />
+      <TextField
+        
+        placeholder="Password"
+        variant="outlined"
+        color="danger"
+      />
+    <Button sx={{background: "rgb(4, 101, 130)",color:"white"}}>Register</Button>
+    </Box>
+  );
 }
-
-export default Register
