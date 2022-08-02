929ed9767985a9eb5b9aa9a1822a3c6e0cb50b23
mehmet <tas349873@gmail.com>
2022-08-02 11:58:49 +0300

    update

--- src/app-router/AppRouter.js
+++ src/app-router/AppRouter.js
@@ -1,9 +1,13 @@
 import { BrowserRouter ,Routes,Route} from "react-router-dom"
 import Navbar from "../components/Navbar"
 import Dashboard from "../pages/Dashboard"
+import Details from "../pages/Details"
+import Login from "../pages/Login"
 import Newblog from "../pages/Newblog"
 import Profile from "../pages/Profile"
 import Register from "../pages/Register"
+import UpdateBlog from "../pages/UpdateBlog"
+import PrivateRouter from "./PrivateRouter"
 
 
 
@@ -15,7 +19,12 @@ const AppRouter = () => {
     <BrowserRouter>
     <Navbar/>
     <Routes>
-        <Route path="/" element = {<Dashboard/>}/>
+        <Route path="/" element = {<PrivateRouter/>}>
+          <Route path="" element={<Dashboard/>}/>
+        </Route>
+        <Route path="/Login" element={<Login/>}
+        <Route path="/details" element={<Details/>}
+        <Route path="/updateblog" element={<UpdateBlog/>}
         <Route path="/profile" element = {<Profile/>}/>
         <Route path="register" element = {<Register/>}/>
         <Route path="/newblog" element = {<Newblog/>}/>
